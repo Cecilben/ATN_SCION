fb903e8fc91bc13e7e4e21b23eeb3a118a8a7527
Cecilben <44363425+Cecilben@users.noreply.github.com>
2019-01-26 16:26:33 +0100

    Update Task 1.go

--- Updated Solution/Task 1.go
+++ Updated Solution/Task 1.go
@@ -7,7 +7,7 @@ package main
 import (
 	
 	"flag"
-	"fmt"
+	"fmt"  
 	"os"
 	"log"
 	"time"
@@ -18,17 +18,14 @@ import (
 	"github.com/scionproto/scion/go/lib/spath"
 )
 
-// Check just ensures the error is nil, or complains and quits
+// Check just ensures the error is nil, or complains and quits 
+//This block has been refered from sensor fetch
 func Check(e error) {
 	if e != nil {
 		fmt.Println("Fatal error. Exiting.", "err", e)
-		os.Exit(1)
 	}
 }
-const (
-	max_i = 10
-	max_trails = 20
-)
+//This block has been refered from sensor fetch
 func printUsage() {
 	fmt.Println(" -s ServerSCIONAddress -c ClientSCIONAddress")
 	fmt.Println("The SCION address is specified as ISD-AS,[IP Address]:Port")
@@ -38,82 +35,62 @@ func main() {
 	var clientAddress string
 	var serverAddress string
 	var err error
-	var local *snet.Addr
-	var remote *snet.Addr
-	var udpConnection *snet.Conn
+	var client_local *snet.Addr
+	var remote_server *snet.Addr
+	var scion_udpconnection *snet.Conn
 
 
 	//Fetches the argument from command line
+	//Refered from sensor fetch
 	flag.StringVar(&clientAddress, "c", "", "Client SCION Address")
 	flag.StringVar(&serverAddress, "s", "", "Server SCION Address")
 	flag.Parse()
 
 	//SCION UDP socket creation
-	
-	if len(clientAddress) == 0
-	{
-		Check(fmt.Errorf("Error, local address needs to be specified with -local"))
-	}
-	else
-	{
+	//Refered from hello world 
+
 	clientAddress, err := snet.AddrFromString(clientAddress)
-		Check(err)
-	}
-	if len(serverAddress) == 0 
-	{
-		Check(fmt.Errorf("Error, remote address needs to be specified with -remote"))
-	}
-	else
-	{
-		serverAddress, err := snet.AddrFromString(serverAddress)
-		Check(err)
-	}
-	
-	dispatcherAddr := "/run/shm/dispatcher/default.sock"
+	Check(err)
+	serverAddress, err := snet.AddrFromString(serverAddress)
+	Check(err)
 
-	snet.Init(local.IA, sciond.GetDefaultSCIONDPath(nil), dispatcherAddr)
+	dispatch := "/run/shm/dispatcher/default.sock"
 
-	udpConnection, err = snet.DialSCION("udp4", local, remote)
+	snet.Init(local.IA, sciond.GetDefaultSCIONDPath(nil), dispatch)
+
+	scion_udpconnection, err = snet.DialSCION("udp4", client_local, remote_server)
 	check(err)
-
+	
+	receivePacketBuffer := make([]byte, 2500) //Intiating a dynamic array of respective size
+	sendPacketBuffer := make([]byte, 16)   //Intiating a dynamic array of respective size
 
 	random_seed := rand.NewSource(time.Now().Unix())
 	
 	var total int64 = 0
-	i := 0 
-	trials := 0 
+	gen_id := rand.New(random_seed).Uint64()   //generating a random ID using rand function 
+	n := binary.PutUvarint(sendPacketBuffer, gen_id)  //Using binary encoder and add the random number
+	sendPacketBuffer[n] = 0
 
-	for i < max_i && trails < max_trails {
-		trails += 1
-
-		gen_id := rand.New(random_seed).Uint64()   //generating a random ID using rand function 
-		n := binary.PutUvarint(sendPacketBuffer, gen_id)  //Using binary encoder and add the random number
-		sendPacketBuffer[n] = 0
-
-		time_sent := time.Now()
-		_, err = udpConnection.Write(sendPacketBuffer)
+	time_sent := time.Now()
+	_, err = scion_udpconnection.Write(sendPacketBuffer)
 		check(err)
 
-		_, _, err = udpConnection.ReadFrom(receivePacketBuffer)
+	_, _, err = scion_udpconnection.ReadFrom(receivePacketBuffer)
 		check(err)
 
-		return_id, n := binary.Uvarint(receivePacketBuffer)
+	return_id, n := binary.Uvarint(receivePacketBuffer)
 		
-		if return_id == gen_id {
-			time_received, _ := binary.Varint(receivePacketBuffer[n:]) // Using the binary encoder library, decoded the value is stored in time recieved 
-			diff := (time_received - time_sent.Unix())
-			total += diff
-			i += 1
+	if return_id == gen_id {
+		time_received, _ := binary.Varint(receivePacketBuffer[n:]) // Using the binary encoder library, decoded the value is stored in time recieved 
+		diff := time_received.sub(time_sent)  	// Difference calculated in seconds
+		total += diff
 		}
 	}
-
-	if i != max_i {
-		check(fmt.Errorf("Error, exceeded maximum number of attempts"))
-	}
-
+	
 	var final_diff float64 = float64(total) / float64(i)
 
 	fmt.Printf("\nClient Address: %s\nServer Address: %s\n", clientAddress, serverAddress);
+	//Printing latency in seconds
 	fmt.Printf("RTT: %.3fs\n", final_diff)
 	fmt.Printf( "Latency: %.3fs\n", final_diff)
 }
