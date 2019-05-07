12fd2497bfd70424d93447a3c897e70647de0e2f
Cecilben <44363425+Cecilben@users.noreply.github.com>
2019-05-07 10:10:16 +0200

    Update sigA_test_.sh

--- SIG_config/sigA_test_.sh
+++ SIG_config/sigA_test_.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo modprobe dummy
-sudo ip link set name dummy11 dev dummy0
+sudo ip link add dummy11 type dummy
 sudo ip addr add 172.16.0.11/32 brd + dev dummy11 label dummy11:0
 sudo ip rule add to 172.16.12.0/24 lookup 11 prio 11
 $SC/bin/sig -config=/home/ubuntu/go/src/github.com/scionproto/scion/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigA.config > $SC/logs/sig${IA}-1.log 2>&1 &
